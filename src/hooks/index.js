a6ca696c68de00e93cc6946c1b89fdbd18ede6c7
bds511 <pypy7000@korea.ac.kr>
2020-02-27 16:41:39 +0900

    add searchs

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,5 +2,3 @@
 export { default as useSiteMetadata } from "./use-site-metadata";
 export { default as useCategoriesList } from "./use-categories-list";
 export { default as useTagsList } from "./use-tags-list";
-export { useOnClickOutside } from "./use-on-click-outside";
-export { useEventListener } from "./use-event-listener";
